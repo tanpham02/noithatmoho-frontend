167373e5e8263ade2cf9381e0cb0220b1edde936
Văn Tân <phamvantan1311@gmail.com>
2023-04-25 09:17:18 +0700

    thrid update

--- src/Components/CreateProduct/CreateProduct.js
+++ src/Components/CreateProduct/CreateProduct.js
@@ -90,7 +90,7 @@ const CreateProduct = () => {
         async function createProduct() {
             const res = await axios.post('https://noithatmoho-backend.up.railway.app/api/products', createPro)
             try {
-                if (res.status === 202) {
+                if (res.status === 'pending' || 202) {
                     setIsLoading(true)
                 } else {
                     window.alert('Thêm mới sản phẩm thành công!')
